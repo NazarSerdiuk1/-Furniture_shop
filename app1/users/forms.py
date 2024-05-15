5defd2e4cac603c26950cefec3c9a36ab717adf5
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-05-15 15:46:50 +0300

    initial commit

---
+++ app1/users/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    # username = forms.CharField(
+    #     label="Ім'я користувача",
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "autofocus": True,
+    #             "class": "form-control",
+    #             "placeholder": "Введіть ваше ім'я користувача",
+    #         }
+    #     )
+    # )
+    # password = forms.CharField(
+    #     label="Пароль",
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "autocomplete": "current-password",
+    #             "class": "form-control",
+    #             "placeholder": "Введіть ваш пароль",
+    #         }
+    #     ),
+    # )
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
